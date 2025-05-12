c79f4f43601bbb80d0eab1d0be12c3f2a9670cb4
Yijia Dai <daiyijia@dhcp-vl2041-2654.eduroam.cornell.edu>
2025-05-12 19:04:07 -0400

    sam api

--- call_sam.py
+++ call_sam.py
@@ -14,14 +14,28 @@ import numpy as np
 # Your ngrok URL
 SERVER_URL = "https://ee30-128-84-100-13.ngrok-free.app"
 
+def encode_image_to_base64(image_path):
+    """Convert image file to base64 string"""
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    return encoded_string
 
-def generate_mask(image, SERVER_URL=SERVER_URL):
+def decode_base64_to_image(base64_string):
+    """Convert base64 string back to PIL Image"""
+    image_data = base64.b64decode(base64_string)
+    image = Image.open(io.BytesIO(image_data))
+    return image
+
+def generate_mask(image_path, SERVER_URL=SERVER_URL):
     """
     Segment image with point prompts
     
     Args:
         image: PIL Image object
     """
+
+    image = encode_image_to_base64(image_path)
+
     data = {
         "image": image,
     }
@@ -31,7 +45,10 @@ def generate_mask(image, SERVER_URL=SERVER_URL):
     
     if response.status_code == 200:
         result = response.json()
-        return result
+        masks = []
+        for mask in result['masks']:
+            masks.append(decode_base64_to_image(mask))
+        return masks
     else:
         print(f"Error: {response.status_code} - {response.text}")
         return None
